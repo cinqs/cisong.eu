875c7cac89db8c7d18790a2277c7ae61d75ce2af
Ubuntu <ubuntu@ip-172-31-37-160.us-west-2.compute.internal>
2015-09-22 23:00:06 +0000

    modified the first page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'Profile',
+  	name: 'Ci SONG' 
+  });
 });
+
+router.get('/profile', function(req, res, next){
+	res.sendFile(path.join(__dirname, '../public/profile.html'));
+})
 
 module.exports = router;
